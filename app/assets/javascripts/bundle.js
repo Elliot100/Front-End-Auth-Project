e56006c322d1a4ef53b7c03a54277950769bc47d
Elly <ellychau10@yahoo.com>
2022-01-01 17:29:49 -0800

    Build actions/session using session utils

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -464,7 +464,7 @@ exports.default = function () {
     _react2.default.createElement(
       "div",
       { className: "hero-img-frame" },
-      _react2.default.createElement("img", { className: "hero-img", src: "https://pmcvariety.files.wordpress.com/2017/02/angry-birds-blues-rovio.jpg?w=1000&h=750&crop=1" })
+      _react2.default.createElement("img", { className: "hero-img", src: "https://variety.com/wp-content/uploads/2017/02/angry-birds-blues-rovio.jpg" })
     ),
     _react2.default.createElement(
       "h1",
--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default () => (
   <div className="home">
     <div className="hero-img-frame">
-      <img className="hero-img" src="https://pmcvariety.files.wordpress.com/2017/02/angry-birds-blues-rovio.jpg?w=1000&h=750&crop=1" />
-    </div> 
+      <img className="hero-img" src="https://variety.com/wp-content/uploads/2017/02/angry-birds-blues-rovio.jpg" />
+    </div>
     <h1>Where birds can chirp.</h1>
   </div>
-)
+);
